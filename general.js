b818101a8cf210e53705fe91bd03bfed738ee6c8
kguardado <kguardado88@gmail.com>
2021-04-02 19:42:43 -0600

    Tercer commit

---
+++ general.js
@@ -0,0 +1,9 @@
+const menu = document.querySelector('#movil');
+const degradado = document.querySelector('#degradado');
+const btnMenu = document.querySelector('#btnMenu')
+
+btnMenu.addEventListener('click', mostrarMenu);
+
+function mostrarMenu (){ 
+    menu.style.left = '0'
+}
